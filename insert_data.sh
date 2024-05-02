29c53a439ed92ee6ede2e16e2699e609fd364a93
alexissanz <66205013+alexissanz@users.noreply.github.com>
2024-05-02 16:53:59 +0100

    Update insert_data.sh

--- insert_data.sh
+++ insert_data.sh
@@ -7,29 +7,18 @@ else
   PSQL="psql --username=freecodecamp --dbname=worldcup -t --no-align -c"
 fi
 
-declare -A team_ids
+# Do not change code above this line. Use the PSQL variable above to query your database.
 
 while IFS=, read -r year round winner opponent winner_goals opponent_goals
 do
-    if [ ! ${team_ids[$winner]} ]
-    then
-        winner_id=$($PSQL "SELECT team_id FROM teams WHERE name = '$winner'")
-        if [ -z "$winner_id" ]
-        then
-            winner_id=$($PSQL "INSERT INTO teams (name) VALUES ('$winner') RETURNING team_id;")
-        fi
-        team_ids[$winner]=$winner_id
-    fi
+  echo "$($PSQL "INSERT INTO teams (name) VALUES ('$winner') ON CONFLICT (name) DO NOTHING")"
+  echo "$($PSQL "INSERT INTO teams (name) VALUES ('$opponent') ON CONFLICT (name) DO NOTHING")"
+done < <(tail -n +2 games.csv)
 
-    if [ ! ${team_ids[$opponent]} ]
-    then
-        opponent_id=$($PSQL "SELECT team_id FROM teams WHERE name = '$opponent'")
-        if [ -z "$opponent_id" ]
-        then
-            opponent_id=$($PSQL "INSERT INTO teams (name) VALUES ('$opponent') RETURNING team_id;")
-        fi
-        team_ids[$opponent]=$opponent_id
-    fi
-
-    $PSQL "INSERT INTO games (year, round, winner_id, opponent_id, winner_goals, opponent_goals) VALUES ($year, '$round', ${team_ids[$winner]}, ${team_ids[$opponent]}, $winner_goals, $opponent_goals);"
+# Agora, vamos inserir os jogos na tabela de jogos
+while IFS=, read -r year round winner opponent winner_goals opponent_goals
+do
+  winner_id="$($PSQL "SELECT team_id FROM teams WHERE name = '$winner'")"
+  opponent_id="$($PSQL "SELECT team_id FROM teams WHERE name = '$opponent'")"
+  echo "$($PSQL "INSERT INTO games (year, round, winner_id, opponent_id, winner_goals, opponent_goals) VALUES ($year, '$round', $winner_id, $opponent_id, $winner_goals, $opponent_goals)")"
 done < <(tail -n +2 games.csv)
